59ef850a350e620f76990b413f62a8861dcc7fd1
barbaramootian <barbara.mootian@student.moringaschool.com>
2022-02-15 16:20:37 +0300

    initiallizin flask application and register blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,40 +1,38 @@
-# from flask import Flask   
-# from config import config_options
-# from flask_bootstrap import Bootstrap
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_login import LoginManager
-# from flask_mail import Mail
+from flask import Flask   
+from config import config_options
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_mail import Mail
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 
-# #........
-# mail = Mail()
+photos=UploadSet('photos',IMAGES)
+#........
+mail = Mail()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+bootstrap = Bootstrap()
+db = SQLAlchemy() 
 
-# def create_app(config_name):
-#     app = Flask(__name__)
-#     #........
-#     mail.init_app(app)
-# login_manager = LoginManager()
-# login_manager.session_protection = 'strong'
-# login_manager.login_view = 'auth.login'
+def create_app(config_name):   
+    # initializing application  
+    app = Flask(__name__)
+    # setting up configuration    
+    app.config.from_object(config_options[config_name])
+   
+    # Initializing flask extensions  
+    bootstrap.init_app(app)
+    mail.init_app(app)  
+    db.init_app(app)     
+    login_manager.init_app(app)
+    configure_uploads(app,photos)
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/authenticate')
 
-# bootstrap = Bootstrap()
-# db = SQLAlchemy() 
+    # Setting config
 
-# def create_app(config_name):   
-#     # initializing application  
-#     app = Flask(__name__)
-#     # setting up configuration    
-#     app.config.from_object(config_options[config_name])
-
-#     # Initializing flask extensions  
-#     bootstrap.init_app(app)  
-#     db.init_app(app)     
-#     login_manager.init_app(app)
-#     # Registering the blueprint
-#     from .main import main as main_blueprint
-#     app.register_blueprint(main_blueprint)
-#     from .auth import auth as auth_blueprint
-#     app.register_blueprint(auth_blueprint, url_prefix='/authenticate')
-
-#     # Setting config
-
-#     return app
+    return app
