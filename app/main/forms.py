832844943b87e289a7ee99f39e986f267403d7b6
barbaramootian <barbara.mootian@student.moringaschool.com>
2022-02-15 15:48:37 +0300

    created different class flaskform

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,20 +1,23 @@
-# from flask_wtf import FlaskForm
-# from wtforms import SubmitField,StringField,PasswordField,BooleanField,SelectField,TextAreaField
+from flask_wtf import FlaskForm
+from wtforms import SubmitField,StringField,PasswordField,BooleanField,SelectField,TextAreaField
 
-# from wtforms.validators import input_required,Email,EqualTo
-# from ..models import User
-# from wtforms import ValidationError
+from wtforms.validators import input_required,Email,EqualTo
+from ..models import User
+from wtforms import ValidationError
 
-# choices=['love','sport','entertainment','travel']
+choices=['love','sport','entertainment','travel','coding']
 
-# class PostForm(FlaskForm):
-#     title=StringField("Post Title",validators=[input_required(message='Title required')],render_kw={"placeholder":"Pitch Title"})
-#     category=SelectField("Post Category",choices=choices,validate_choice=True)
-#     content=StringField("Post body",validators=[input_required(message='body required')],render_kw={"placeholder":"Pitch body"})
-#     submit=SubmitField("POST PITCH")
+class PostForm(FlaskForm):
+    title=StringField("Post Title",validators=[input_required(message='Title required')],render_kw={"placeholder":"Pitch Title"})
+    category=SelectField("Post Category",choices=choices,validate_choice=True)
+    content=StringField("Post body",validators=[input_required(message='body required')],render_kw={"placeholder":"Pitch body"})
+    submit=SubmitField("POST PITCH")  
 
-# class CommentForm(FlaskForm):
+class CommentForm(FlaskForm):
 
+     comment=StringField("Pitch comment",validators=[input_required(message='body required')],render_kw={"placeholder":"Pitch comment"}) 
+     submit=SubmitField("Comment")
 
-#      comment=StringField("Pitch comment",validators=[input_required(message='body required')],render_kw={"placeholder":"Pitch comment"}) 
-#      submit=SubmitField("Comment")
+class UpdateForm(FlaskForm):                                                                                       
+    bio = TextAreaField("Your Biography", validators=[input_required(message='Biography field is required')])
+    submit = SubmitField("Update Profile")       
