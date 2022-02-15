d4e3b8c146da96aebb68191d3b25f6061a3cda4b
barbaramootian <barbara.mootian@student.moringaschool.com>
2022-02-15 16:23:23 +0300

    add request function

--- app/requests.py
+++ app/requests.py
@@ -1,11 +1,11 @@
-# import urllib.request,json
-# def get_quote():
-#     '''
-#     Function that gets the random quotes
-#     '''
-#     get_article_details_url = 'http://quotes.stormconsultancy.co.uk/random.json'.format()
-#     with urllib.request.urlopen(get_article_details_url) as url:
-#         quote_data = url.read()
-#         quote_response = json.loads(quote_data)
-#         print(quote_response)
-#     return quote_response
+import urllib.request,json
+def get_quote():
+    '''
+    Function that gets the random quotes
+    '''
+    get_article_details_url = 'http://quotes.stormconsultancy.co.uk/random.json'.format()
+    with urllib.request.urlopen(get_article_details_url) as url:
+        quote_data = url.read()
+        quote_response = json.loads(quote_data)
+        print(quote_response)
+    return quote_response
